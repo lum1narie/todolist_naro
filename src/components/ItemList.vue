f108f820df79096ef1de1ab4f79e5010335cfd27
lum1narie <sn.lum.fallenrose.aol@gmail.com>
2021-05-25 18:10:27 +0900

    商品リストの雛形を追加

---
+++ src/components/ItemList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div>ItemList</div>
+    <div v-for="item in items" :key="item.name">
+      <div class="item">
+        <div class="name">名前: {{ item.name }}</div>
+        <div class="price">{{ item.price }} 円</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const items = ref([
+      { name: "たまご", price: 100 },
+      { name: "りんご", price: 160 },
+    ]);
+    return {items};
+  },
+};
+</script>
+
+<style></style>
